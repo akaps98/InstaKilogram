8d9444d429c0a035e7d40c2a18d16baaa2fcc9b9
beevo <bee.vo@contemi.com>
2022-05-02 19:55:42 +0700

    switch to php -s command

--- www/php/navigation-handler.php
+++ www/php/navigation-handler.php
@@ -1,11 +1,16 @@
 <?php
 function navigate($target)
 {
-    $home_page = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]" . "/InstaKilogram/www";
-    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $home_page = getHost();
+    $actual_link = getHost() . "$_SERVER[REQUEST_URI]";
+
     if (!$target) return $home_page;
     if (strpos($actual_link, $target)) {
         return $actual_link;
     }
     return $home_page . $target;
+}
+
+function getHost(){
+    return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 }
