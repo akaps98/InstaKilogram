d9404ac3282551f9ecd30dd8b03a03fbf3ddc637
minhboro197 <minhvuviet2000@gmail.com>
2022-05-04 12:12:56 +0700

    properly wired for view details button

--- www/php/user-listing-handler.php
+++ www/php/user-listing-handler.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . "csv-handler.php";
 require_once __DIR__ . DIRECTORY_SEPARATOR. "navigation-handler.php";
+require_once __DIR__ . DIRECTORY_SEPARATOR. "upload-handler.php";
 
 function renderListing($filePath = __DIR__ . DIRECTORY_SEPARATOR . '..'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'users.csv')
 {
@@ -17,10 +18,21 @@ function renderListing($filePath = __DIR__ . DIRECTORY_SEPARATOR . '..'.DIRECTOR
         echo '<th scope="col">' . $data[$x][2] . '</th>';
         echo '<th scope="col">' . $data[$x][5] . '</th>';
         if ($filePath === __DIR__ .  DIRECTORY_SEPARATOR . '..' .DIRECTORY_SEPARATOR .'data'. DIRECTORY_SEPARATOR.'users.csv') {
-            echo '<th scope="col"> <button> <a> View Detail</a>  </button> </th>';
+            echo '<th scope="col"> <button type="submit" name="q-email" value="'.$data[$x][3].'">View details</button> </th>';
         }
         echo '</tr>';
     }
 }
-
+function directToUserProfile(){
+    if ($_SERVER["REQUEST_METHOD"] == "GET"){
+        if(isset($_GET['q-email'])){
+            $email = $_GET['q-email'];
+            $user = getUser(__DIR__ . '\..\data\users.csv', $email);
+            $_SESSION['logged'] = $user[0];
+            $_SESSION['userType'] = $user[5];
+            header("Location: ../user/profile.php");
+            exit();
+        }
+    } 
+}
 ?>
