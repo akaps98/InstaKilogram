90c4111f0f7c48c01afdfde4e86c58af61875d90
beevo <bee.vo@contemi.com>
2022-05-08 18:37:03 +0700

    fix: navigation errors

--- www/templates/common-share/header.php
+++ www/templates/common-share/header.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . DIRECTORY_SEPARATOR. "..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."php".DIRECTORY_SEPARATOR."session-handler.php";
 require_once __DIR__ . DIRECTORY_SEPARATOR. "..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."php".DIRECTORY_SEPARATOR."auth-handler.php";
 require_once __DIR__ . DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."php".DIRECTORY_SEPARATOR."navigation-handler.php";
 if (isset($_GET['logout'])) {
