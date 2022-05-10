d66430c74d3d26032554887ca48c8dcbb17e3b3e
akaps98 <fc0302@naver.com>
2022-05-11 01:03:03 +0700

    feat: implement header/footer

---
+++ www/js/footer.js
@@ -0,0 +1,13 @@
+function overlay1() {
+    document.querySelector("#popup_copyright").style.display = "block";
+}
+
+function overlay2(){
+    document.querySelector("#popup_help").style.display = "block";
+}
+
+function off() {
+    document.querySelector("#popup_copyright").style.display = "none";
+    document.querySelector("#popup_help").style.display = "none";
+}
+  
