f655ef009c15b0e9baca27a07729f7b47735bca5
bee <vogiabao292@gmail.com>
2022-05-13 23:56:18 +0700

    merge: new header and footer from Tonian Branch

--- www/templates/footer-contents/privacy.php
+++ www/templates/footer-contents/privacy.php
@@ -1,9 +1,5 @@
-<?php
-require_once __DIR__. DIRECTORY_SEPARATOR. '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'session-handler.php';
-require_once __DIR__. DIRECTORY_SEPARATOR.'..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'navigation-handler.php';
-require_once __DIR__. DIRECTORY_SEPARATOR.'..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'auth-handler.php';
+<?php include(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'session-handler.php')
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,92 +7,102 @@ require_once __DIR__. DIRECTORY_SEPARATOR.'..' . DIRECTORY_SEPARATOR . '..' . DI
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../../css/footer.css" type="text/css">
     <link rel="stylesheet" href="../../css/privacy.css" type="text/css">
 </head>
 <body>
-    <header>
-        <?php include('./templates/common-share/header.php')?>
-    </header>
-    <main>
-        <div class="title">
-            <h1><b>Privacy and Security Policy</b></h1>
-        </div>
-        <div class="content">
-            <h5>The personal information you provide on Instakilogram is processed by the Instagram website and the Instakilogram service. 
-                We would like to inform you about the safe use of your personal information and the protection of your data.
-                As described in this policy, you agree to use your personal information on Instakilogram. 
-                If you do not agree with this policy, please do not use the site or provide personal data to Instakilogram.</h5>
-        </div>
-        <div class="content_title">
-            <h3>Collection of Personal Data<h3>
-        </div>
-        <div class="content">
-            <h5>In general, you may visit this site without providing personal information. 
-                Personal information submitted to Instakilogram(name, phone number, email address, picture) 
-                may be collected by Instakilogram.</h5>
-        </div>
-        <div class="content_title">
-            <h3>Purpose of Use<h3>
-        </div>
-        <div class="content">
-            <h5>Instakilogram can be used by requesting your personal information to support the use of our products. 
-                Instakilogram can collect and use your personal information and surveys 
-                to better improve your customers' needs and products.</h5>
-        </div>
-        <div class="content_title">
-            <h3>Transfer or Disclosure of Personal data<h3>
-        </div>
-        <div class="content">
-            <h5>Instakilogram does not provide personal information to third parties for direct marketing purposes.<br><br>
+<header>
+    <?php include(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'common-share' . DIRECTORY_SEPARATOR . 'header.php') ?>
+</header>
+<main>
+    <div class="title">
+        <h1><b>Privacy and Security Policy</b></h1>
+    </div>
+    <div class="content">
+        <h5>The personal information you provide on Instakilogram is processed by the Instagram website and the
+            Instakilogram service.
+            We would like to inform you about the safe use of your personal information and the protection of your data.
+            As described in this policy, you agree to use your personal information on Instakilogram.
+            If you do not agree with this policy, please do not use the site or provide personal data to
+            Instakilogram.</h5>
+    </div>
+    <div class="content_title">
+        <h3>Collection of Personal Data<h3>
+    </div>
+    <div class="content">
+        <h5>In general, you may visit this site without providing personal information.
+            Personal information submitted to Instakilogram(name, phone number, email address, picture)
+            may be collected by Instakilogram.</h5>
+    </div>
+    <div class="content_title">
+        <h3>Purpose of Use<h3>
+    </div>
+    <div class="content">
+        <h5>Instakilogram can be used by requesting your personal information to support the use of our products.
+            Instakilogram can collect and use your personal information and surveys
+            to better improve your customers' needs and products.</h5>
+    </div>
+    <div class="content_title">
+        <h3>Transfer or Disclosure of Personal data<h3>
+    </div>
+    <div class="content">
+        <h5>Instakilogram does not provide personal information to third parties for direct marketing purposes.<br><br>
 
-                Instakilogram may request that its subsidiary and its distributor register as a new user to access the services 
-                you have requested, and this material may be transferred. 
-                These data transfers are sent to the service provider to make a service request. 
-                For example, it is provided for customer service such as AS.<br><br>
-                
-                Instakilogram may provide personal information on search warrants or other legally valid requests or orders.</h5>
-        </div>
-        <div class="content_title">
-            <h3>Cookies<h3>
-        </div>
-        <div class="content">
-            <h5>When you visit this site, you may store some data in the form of a "cookie" 
-                that automatically recognizes your computer on your computer. 
-                Cookies are used for better service and to improve the site. 
-                If you don't want to receive cookies, you can set it up in your web browser.</h5>
-        </div>
-        <div class="content_title">
-            <h3>Security<h3>
-        </div>
-        <div class="content">
-            <h5>Instakilogram will only disclose all personal information collected from Instakilogram's website 
-                and to service providers who need the information described in our policies for confidentiality.</h5>
-        </div>
-        <div class="content_title">
-            <h3>Access to Your Personal Data and Correction of Data<h3>
-        </div>
-        <div class="content">
-            <h5>To review and resolve your personal information, Instakilogram will respond 
-                if you wish to correct inaccuracies or delete your personal information.<br>
-                If you want to delete or modify it, please send an email to the address below.<h5><br>
-            <h6>lgtwins0302@gmail.com<br><br>
-                fc0302@naver.com<j6>
-        </div>
-        <div class="content_title">
-            <h3>Changes to This Policy<h3>
-        </div>
-        <div class="content">
-            <h5>Instakilogram may make changes to this policy at any time and any changes will be posted immediately on the site. 
-                If you do not agree to the revision of the policy, 
-                please contact us by e-mail or general mail at the contact address above. 
-                If you continue to use this Site, you are considered to agree to this policy.</h5><br>
-        </div>
-    </main>
-    <footer>
-        <?php include('./templates/common-share/footer.php')?>
-    </footer>
+            Instakilogram may request that its subsidiary and its distributor register as a new user to access the
+            services
+            you have requested, and this material may be transferred.
+            These data transfers are sent to the service provider to make a service request.
+            For example, it is provided for customer service such as AS.<br><br>
+
+            Instakilogram may provide personal information on search warrants or other legally valid requests or orders.
+        </h5>
+    </div>
+    <div class="content_title">
+        <h3>Cookies<h3>
+    </div>
+    <div class="content">
+        <h5>When you visit this site, you may store some data in the form of a "cookie"
+            that automatically recognizes your computer on your computer.
+            Cookies are used for better service and to improve the site.
+            If you don't want to receive cookies, you can set it up in your web browser.</h5>
+    </div>
+    <div class="content_title">
+        <h3>Security<h3>
+    </div>
+    <div class="content">
+        <h5>Instakilogram will only disclose all personal information collected from Instakilogram's website
+            and to service providers who need the information described in our policies for confidentiality.</h5>
+    </div>
+    <div class="content_title">
+        <h3>Access to Your Personal Data and Correction of Data<h3>
+    </div>
+    <div class="content">
+        <h5>To review and resolve your personal information, Instakilogram will respond
+            if you wish to correct inaccuracies or delete your personal information.<br>
+            If you want to delete or modify it, please send an email to the address below.
+            <h5><br>
+                <h6>lgtwins0302@gmail.com<br><br>
+                    fc0302@naver.com
+                    <j6>
+    </div>
+    <div class="content_title">
+        <h3>Changes to This Policy<h3>
+    </div>
+    <div class="content">
+        <h5>Instakilogram may make changes to this policy at any time and any changes will be posted immediately on the
+            site.
+            If you do not agree to the revision of the policy,
+            please contact us by e-mail or general mail at the contact address above.
+            If you continue to use this Site, you are considered to agree to this policy.</h5><br>
+    </div>
+</main>
+<footer>
+    <?php include(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'common-share' . DIRECTORY_SEPARATOR . 'footer.php') ?>
+</footer>
 </body>
 </html>
